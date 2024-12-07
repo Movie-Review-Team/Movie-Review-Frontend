9e1635768034225af7b58a482ae1db37a99d18a5
kyungsoo <yksoo00@naver.com>
2024-12-08 02:14:43 +0900

    chore: 기본 셋팅 추가

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import TodoList from './pages/TodoList';
+import MovieListPage from './pages/MovieListPage';
+import AddMoviePage from './pages/AddMoviePage';
+import EditMoviePage from './pages/EditMoviePage';
+import MovieDetailPage from './pages/MovieDetailPage';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<TodoList />} />
+        <Route path="/" element={<MovieListPage />} />
+        <Route path="/movies/add" element={<AddMoviePage />} />
+        <Route path="/movies/edit/:id" element={<EditMoviePage />} />
+        <Route path="/movies/:id" element={<MovieDetailPage />} />
       </Routes>
     </Router>
   );
